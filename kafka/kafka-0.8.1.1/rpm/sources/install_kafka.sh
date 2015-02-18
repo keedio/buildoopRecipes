69c9575dfeb38617c23b27be503e4fbbbbc5257d
mvalleavila <mvalleavila@gmail.com>
2015-02-18 13:18:50 +0100

    Added slf4j-log4j12-1.7.2.jar to avoid KAFKA-1354

--- kafka/kafka-0.8.1.1/rpm/sources/install_kafka.sh
+++ kafka/kafka-0.8.1.1/rpm/sources/install_kafka.sh
@@ -96,6 +96,12 @@ cp -ra ${BUILD_DIR}/core/build/dependant-libs-2.10.4/*.jar $PREFIX/$LIB_DIR/libs
 cp -ra ${BUILD_DIR}/bin/*.sh $PREFIX/$LIB_DIR/bin
 ln -s /etc/kafka/conf $PREFIX/$LIB_DIR/config 
 
+# Get slf4j-log4j12.jar to avoid StaticBindingError (KAFKA-1354)
+cd ${RPM_SOURCE_DIR}
+wget http://central.maven.org/maven2/org/slf4j/slf4j-log4j12/1.7.2/slf4j-log4j12-1.7.2.jar
+cp slf4j-log4j12-1.7.2.jar $PREFIX/$LIB_DIR/libs
+cd -
+
 # Copy in the configuration files
 install -d -m 0755 $PREFIX/$CONF_DIR
 cp -a ${RPM_SOURCE_DIR}/conf.dist/* $PREFIX/$CONF_DIR
